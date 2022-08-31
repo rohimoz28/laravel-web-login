bbccf2f5a6b58572f32ca4d976b6a9d34504b0dd
rohimoz28 <rohimoz28@gmail.com>
2022-08-31 14:11:23 +0700

    add attendance sidebar

--- resources/views/templates/app/sidebar.blade.php
+++ resources/views/templates/app/sidebar.blade.php
@@ -10,6 +10,23 @@
     </ul>
 
     <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
+      <span>Attendance</span>
+    </h6>
+    <ul class="nav flex-column mb-2">
+      <li class="nav-item">
+        <a class="nav-link {{ Request::is('user/profile/*') ? 'active' : '' }}" href="/attendance/absence/{{ auth()->user()->id }}">
+          <span data-feather="file-text" class="align-text-bottom"></span>
+         Absence 
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link {{ Request::is('user/password/*') ? 'active' : '' }}" href="/attendance/attendance-list/{{ auth()->user()->id }}">
+          <span data-feather="file-text" class="align-text-bottom"></span>
+         Attendance List 
+        </a>
+      </li>
+    </ul>
+    <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
       <span>Settings</span>
     </h6>
     <ul class="nav flex-column mb-2">
